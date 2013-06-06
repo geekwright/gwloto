c9445d16704f6bb9f1e684f5fd2293b872acbaa2
rgriffith <rgriffith@d16e68f3-ae12-4566-a441-7357fac79f6f>
2013-06-06 15:03:56 +0000

    Update icons, use XOOPS standard icons if available

--- admin/menu.php
+++ admin/menu.php
@@ -4,13 +4,48 @@
 *
 * This file is part of gwloto - geekwright lockout tagout
 *
-* @copyright  Copyright © 2010 geekwright, LLC. All rights reserved. 
+* @copyright  Copyright © 2010 geekwright, LLC. All rights reserved.
 * @license    gwloto/docs/license.txt  GNU General Public License (GPL)
 * @since      1.0
 * @author     Richard Griffith <richard@geekwright.com>
 * @package    gwloto
 * @version    $Id$
 */
+
+if(file_exists(XOOPS_ROOT_PATH.'/Frameworks/moduleclasses/icons/32/about.png')) {
+$pathIcon32='../../Frameworks/moduleclasses/icons/32';
+
+$adminmenu[1] = array(
+	'title' => _MI_GWLOTO_ADMENU,
+	'link'  => 'admin/index.php',
+	'icon'  => $pathIcon32.'/home.png'
+);
+
+$adminmenu[] = array(
+	'title' => _MI_GWLOTO_ADMENU_ABOUT,
+	'link'  => 'admin/about.php',
+	'icon'  => $pathIcon32.'/about.png'
+);
+
+$adminmenu[] = array(
+	'title' => _MI_GWLOTO_ADMENU_PLACE,
+	'link'  => 'admin/addplace.php',
+	'icon'  => 'images/admin/places.png'
+);
+
+$adminmenu[] = array(
+	'title' => _MI_GWLOTO_ADMENU_LANG,
+	'link'  => 'admin/language.php',
+	'icon'  => 'images/admin/languages.png'
+);
+
+$adminmenu[] = array(
+	'title' => _MI_GWLOTO_ADMENU_PLUGINS,
+	'link'  => 'admin/plugins.php',
+	'icon'  => 'images/admin/plugins.png'
+);
+
+} else {
 
 $adminmenu[1] = array(
 	'title' => _MI_GWLOTO_ADMENU,
@@ -42,4 +77,5 @@ $adminmenu[] = array(
 	'icon'  => 'images/admin/plugins.png'
 );
 
+}
 ?>
